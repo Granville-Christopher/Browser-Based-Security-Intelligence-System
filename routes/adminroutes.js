98fece1f6e2e82a66692d92fc972f01bda5c451a
Granville Christopher <christophergranville2@gmail.com>
2025-07-08 16:25:59 -0700

    final auth debugging

--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -8,6 +8,8 @@ const reverseGeocode = require("../utils/reversegeocode");
 const router = require("express").Router();
 const getOtpController = require("../controllers/admin/getotpcontroller");
 const resetPasswordController = require("../controllers/admin/resetpasswordcontroller");
+const bruteforceAttackLogs = require("../models/bruteforcelog");
+const PasswordResetLog = require("../models/passwordresetlog");
 
 // IP extraction utility
 const getClientIp = (req) => {
@@ -63,6 +65,80 @@ router.get("/resetpassword", (req, res) => {
   return res.status(200).render("admin/resetpassword", {
     title: "Admin - Signup",
     message: "Admin signup",
+  });
+});
+
+router.get("/passwordresetlogs", async (req, res) => {
+  try {
+    const logs = await PasswordResetLog.find()
+      .sort({ createdAt: -1 })
+      .limit(100);
+    if (!logs || logs.length === 0) {
+      return res.status(404).render("admin/password-reset-logs", {
+        title: "Admin - Password Reset Logs",
+        message: "No password reset logs found.",
+      });
+    }
+
+    return res.status(200).render("admin/password-reset-logs", {
+      title: "Admin - Password Reset Logs",
+      message: "Admin Password Reset Logs",
+      logs,
+    });
+  } catch (err) {
+    console.error("❌ Error fetching logs:", err);
+    return res.status(500).render("admin/password-reset-logs", {
+      title: "Admin - Password Reset Logs",
+      message: "Something went wrong.",
+      logs: [],
+    });
+  }
+});
+
+router.get("/bruteforceattacklogs", async (req, res) => {
+  try {
+    const logs = await bruteforceAttackLogs
+      .find()
+      .sort({ createdAt: -1 })
+      .limit(100);
+    if (!logs || logs.length === 0) {
+      return res.status(404).render("admin/bruteforce-logs", {
+        title: "Admin - Brute Force Attack Logs",
+        message: "No brute force attack logs found.",
+        logs: [],
+      });
+    }
+    return res.status(200).render("admin/bruteforce-logs", {
+      title: "Admin - Brute Force Attack Logs",
+      message: "Admin Brute Force Attack Logs",
+      logs,
+    });
+  } catch (err) {
+    console.error("❌ Error fetching logs:", err);
+    return res.status(500).render("admin/bruteforce-logs", {
+      title: "Admin - Brute Force Attack Logs",
+      message: "Something went wrong.",
+      logs: [],
+    });
+  }
+});
+
+router.get("/authenticateuser", (req, res) => {
+  return res.status(200).render("admin/authenticate-user", {
+    title: "Admin - Authenticate User",
+    message: "Admin Authenticate User",
+  });
+});
+router.get("/userlogs", (req, res) => {
+  return res.status(200).render("admin/user-logs", {
+    title: "Admin - User Logs",
+    message: "Admin User Logs",
+  });
+});
+router.get("/settings", (req, res) => {
+  return res.status(200).render("admin/settings", {
+    title: "Admin - Settings",
+    message: "Admin Settings",
   });
 });
 
